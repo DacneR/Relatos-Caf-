39494fbee0d630d8d5a399e33ecb56c181e92cc2
Dacner <dacnerpiedrahita@gmail.com>
2025-01-23 14:57:19 -0500

    Creación de carrusel

--- public/js/java.js
+++ public/js/java.js
@@ -9,3 +9,34 @@ function togglePassword() {
     }
 }
 
+//Carrusel inicio
+let slideIndex = 0;
+let slideInterval;
+
+// Llamar a showSlides() cuando se carga la página para iniciar el carrusel
+document.addEventListener("DOMContentLoaded", function() {
+    showSlides();
+});
+
+function showSlides() {
+    let slides = document.getElementsByClassName("carousel-item");
+    let dots = document.getElementsByClassName("dot");
+
+    // Ocultar todos los slides y remover la clase "active" de los dots
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    // Mostrar el slide actual y marcar el dot correspondiente como activo
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "flex";  
+    dots[slideIndex - 1].className += " active";
+
+    // Reiniciar el intervalo para cambiar de slide cada 5 segundos
+    slideInterval = setTimeout(showSlides, 5000); 
+}
+//Carrusel final
